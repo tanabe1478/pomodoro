5a7e1f3753fec96ba18fc46e6f776a536bd41cd1
tanabe1478 <tanabe.1478@gmail.com>
2022-06-20 00:17:56 +0900

    画面一覧を管理するディレクトリを作成して表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pomodoro/pages/pages.dart';
 import 'package:pomodoro/providers/providers.dart';
 import 'package:pomodoro/theme_selector_provider.dart';
 
@@ -20,46 +21,6 @@ class MyApp extends ConsumerWidget {
       darkTheme: darkThemeData,
       themeMode: ref.read(themeSelectorProvider),
       home: HomePage(title: appName),
-    );
-  }
-}
-
-class HomePage extends ConsumerWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final pomodoroTimer = ref.watch(pomodoroTimerProvider);
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.settings),
-            onPressed: () => {},
-          ),
-        ],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              'Count: ${pomodoroTimer.count}',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(pomodoroTimerProvider.notifier).increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
